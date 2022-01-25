30c9bbb436cc94bd637aacfe53ad3a637b8b693a
aimadkha <aimadkha@gmail.com>
2022-01-25 10:39:13 +0100

    add project to repo

---
+++ src/main/java/net/manage_employee/registration/controller/AdminServlet.java
@@ -0,0 +1,59 @@
+package net.manage_employee.registration.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import net.manage_employee.registration.dao.EmployeeDao;
+import net.manage_employee.registration.repository.EmployeeRepositoryImp;
+import net.manage_employee.registration.repository.IEmployeeRepository;
+import net.manage_employee.registration.thymleaf.TemplateEngineUtil;
+
+/**
+ * Servlet implementation class AdminServlet
+ */
+@WebServlet("/employee")
+public class AdminServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private EmployeeDao employeeDao;
+	private IEmployeeRepository empRepo;
+	TemplateEngine engine;
+	WebContext context;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminServlet() {
+    	this.employeeDao = new EmployeeDao();
+    	this.empRepo = new EmployeeRepositoryImp();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+		this.context = new WebContext(request, response, request.getServletContext());
+		response.setCharacterEncoding("utf-8");
+		engine.process("index.html", context, response.getWriter());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+
+}
